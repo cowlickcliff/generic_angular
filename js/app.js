95a4cf6840396fb5dfa4746c1c7c93f76564b6d8
bebe <bebe@bebe-lappy.(none)>
2013-06-20 23:50:06 -0400

    Added a carousel inside of the toggle div

--- js/app.js
+++ js/app.js
@@ -26,10 +26,6 @@ app.config(function($routeProvider) {
 });
 
 
-function CollapseDemoCtrl($scope) {
-    console.log("isCollapsed: " + $scope.isCollapsed);
-    $scope.isCollapsed = false;
-}
 
 
 
@@ -117,3 +113,25 @@ app.factory("ChooseUserCaseService", function() {
 
 
 
+/*  Adding some bootstrap stuff */
+function CollapseDemoCtrl($scope) {
+    $scope.isCollapsed = false;
+}
+
+
+function CarouselDemoCtrl($scope) {
+    $scope.myInterval = 5000;
+  $scope.slides = [
+      {image: 'http://placekitten.com/200/200',text: 'Kitten.'},
+      {image: 'http://placekitten.com/225/200',text: 'Kitty!'},
+      {image: 'http://placekitten.com/250/200',text: 'Cat.'},
+      {image: 'http://placekitten.com/275/200',text: 'Feline!'}
+  ];
+    $scope.addSlide = function() {
+	$scope.slides.push({
+	    image: 'http://placekitten.com/'+(200+25*Math.floor(Math.random()*4))+'/200',
+	    text: ['More','Extra','Lots of','Surplus'][Math.floor(Math.random()*4)] + ' ' +
+		['Cats', 'Kittys', 'Felines', 'Cutes'][Math.floor(Math.random()*4)]
+	});
+    };
+}
